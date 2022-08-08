7bc44d53580b0fc0b5d2ae45369a156fe2ff168f
Moe <moe@chocola.dev>
2022-08-08 14:56:25 +0700

    Fix Layout Bug

--- lib/screens/resume.dart
+++ lib/screens/resume.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:moepoi_site/components/resume_experience.dart';
 import 'package:moepoi_site/components/resume_education.dart';
 import 'package:moepoi_site/components/resume_language.dart';
-import 'package:moepoi_site/utils/utils.dart' show HideScrollGlow, kDesktopBreakPoint, kMobileBreakpoint, kTabletBreakpoint;
+import 'package:moepoi_site/utils/utils.dart'
+    show
+        HideScrollGlow,
+        kDesktopBreakPoint,
+        kMobileBreakpoint,
+        kTabletBreakpoint;
 
 class Resume extends StatefulWidget {
   const Resume({Key? key}) : super(key: key);
@@ -19,89 +24,95 @@ class _ResumeState extends State<Resume> {
       title: 'Moe Poi | Resume',
       color: const Color.fromARGB(255, 80, 192, 144),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Resume', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700, fontSize: 40.0),),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          automaticallyImplyLeading: false,
-          actions: [
-            Container(
-              margin: const EdgeInsets.only(right: 20),
-              child: IconButton(
-                icon: const Icon(Icons.close, color: Color.fromARGB(255, 80, 192, 144),),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
+          appBar: AppBar(
+            title: const Text(
+              'Resume',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 40.0),
             ),
-          ],
-        ),
-        body: LayoutBuilder(builder: (context, dimension) {
-          Widget desktop = ScrollConfiguration(
-            behavior: HideScrollGlow(),
-            child: ListView(
-              shrinkWrap: true,
-              children: [
-                const SizedBox(height: 50.0,),
-                SizedBox(
-                  height: 500,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Flexible(
-                        flex: 1,
-                        child: ResumeExperience(),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Flexible(flex: 1, child: ResumeEducation()),
-                            Flexible(flex: 2, child: ResumeLanguage()),
-                          ],
-                        ),
-                      )
-                    ]
-                  )
-                )
-              ]
-            )
-          );
-    
-          Widget mobile = ScrollConfiguration(
-            behavior: HideScrollGlow(),
-            child: Center(
-              child: ListView(
-                // crossAxisCount: 1,
-                padding: const EdgeInsets.all(15.0),
-                shrinkWrap: true,
-                // childAspectRatio: 0.8,
-                children: const [
-                  ResumeExperience(),
-                  SizedBox(height: 30.0,),
-                  ResumeEducation(),
-                  SizedBox(height: 30.0,),
-                  ResumeLanguage(),
-                ]
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            automaticallyImplyLeading: false,
+            actions: [
+              Container(
+                margin: const EdgeInsets.only(right: 20),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.close,
+                    color: Color.fromARGB(255, 80, 192, 144),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
               ),
-            )
-          );
-    
-          if (dimension.maxWidth <= kMobileBreakpoint) {
-            return mobile;
-          } else if (dimension.maxWidth > kMobileBreakpoint &&
-              dimension.maxWidth <= kTabletBreakpoint) {
-            return desktop;
-          } else if (dimension.maxWidth > kTabletBreakpoint &&
-              dimension.maxWidth <= kDesktopBreakPoint) {
-            return desktop;
-          } else {
-            return desktop;
-          }
-        })
-      ),
+            ],
+          ),
+          body: LayoutBuilder(builder: (context, dimension) {
+            Widget desktop = ScrollConfiguration(
+                behavior: HideScrollGlow(),
+                child: ListView(shrinkWrap: true, children: [
+                  const SizedBox(
+                    height: 50.0,
+                  ),
+                  SizedBox(
+                      height: 500,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Flexible(
+                              flex: 1,
+                              child: ResumeExperience(),
+                            ),
+                            Flexible(
+                              flex: 1,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Flexible(flex: 1, child: ResumeEducation()),
+                                  Flexible(flex: 1, child: ResumeLanguage()),
+                                ],
+                              ),
+                            )
+                          ]))
+                ]));
+
+            Widget mobile = ScrollConfiguration(
+                behavior: HideScrollGlow(),
+                child: Center(
+                  child: ListView(
+                      // crossAxisCount: 1,
+                      padding: const EdgeInsets.all(15.0),
+                      shrinkWrap: true,
+                      // childAspectRatio: 0.8,
+                      children: const [
+                        ResumeExperience(),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        ResumeEducation(),
+                        SizedBox(
+                          height: 30.0,
+                        ),
+                        ResumeLanguage(),
+                      ]),
+                ));
+
+            if (dimension.maxWidth <= kMobileBreakpoint) {
+              return mobile;
+            } else if (dimension.maxWidth > kMobileBreakpoint &&
+                dimension.maxWidth <= kTabletBreakpoint) {
+              return desktop;
+            } else if (dimension.maxWidth > kTabletBreakpoint &&
+                dimension.maxWidth <= kDesktopBreakPoint) {
+              return desktop;
+            } else {
+              return desktop;
+            }
+          })),
     );
   }
 }
