7b50f1c50f90bfba5dac5a285da8b7203c2b6fef
Moe <moe@chocola.dev>
2022-10-11 14:06:30 +0700

    Add Kitchen Counts

--- lib/data/projects.dart
+++ lib/data/projects.dart
@@ -4,7 +4,8 @@ var projectsData = [
     'image': 'assets/images/projects/nepgear.webp',
     'date': '23 November 2018',
     'categories': 'Bot',
-    'description': 'Telegram bot that allows you to search for your favorite Anime, Manga, Character with other useful features !!!',
+    'description':
+        'Telegram bot that allows you to search for your favorite Anime, Manga, Character with other useful features !!!',
     'url': 'https://t.me/NepgearBot'
   },
   {
@@ -12,7 +13,8 @@ var projectsData = [
     'image': 'assets/images/projects/whois.webp',
     'date': ' 3 September 2019',
     'categories': 'Bot',
-    'description': 'Telegram bot that provide Information about domain, ip, and web hosting.',
+    'description':
+        'Telegram bot that provide Information about domain, ip, and web hosting.',
     'url': 'https://t.me/WhoisBot'
   },
   {
@@ -28,7 +30,8 @@ var projectsData = [
     'image': 'assets/images/projects/toaru-stickers.webp',
     'date': '13 May 2020',
     'categories': 'App',
-    'description': 'Toaru Stickers Application for Whatsapp. Build with Flutter :)',
+    'description':
+        'Toaru Stickers Application for Whatsapp. Build with Flutter :)',
     'url': 'https://github.com/moepoi/Toaru-Stickers'
   },
   {
@@ -36,7 +39,8 @@ var projectsData = [
     'image': 'assets/images/projects/neonime-app.webp',
     'date': '5 August 2020',
     'categories': 'App',
-    'description': 'App for streaming & download anime (Indo sub). Build with Flutter :)',
+    'description':
+        'App for streaming & download anime (Indo sub). Build with Flutter :)',
     'url': 'https://github.com/moepoi/Neonime-App'
   },
   {
@@ -62,5 +66,13 @@ var projectsData = [
     'categories': 'App',
     'description': 'Native application for Nekoya ~',
     'url': 'https://github.com/Nekoya-Site/App'
-  }
+  },
+  {
+    'title': 'Kitchen Counts',
+    'image': 'assets/images/projects/kitchen-counts.webp',
+    'date': '6 October 2022',
+    'categories': 'App',
+    'description': 'Stock management app for kitchen.',
+    'url': 'https://github.com/moepoi/Kitchen_Counts'
+  },
 ];
