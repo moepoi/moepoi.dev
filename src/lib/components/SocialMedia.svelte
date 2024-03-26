9f0f9a631a53dd959c20dae80b9123e69362bebd
Moe Poi ~ <moe@poi.lol>
2024-03-27 01:56:25 +0700

    Update social media icons with svg

--- src/lib/components/SocialMedia.svelte
+++ src/lib/components/SocialMedia.svelte
@@ -1,14 +1,21 @@
+<script lang="ts">
+  import telegram from '../assets/telegram.svg';
+  import mastodon from '../assets/mastodon.svg';
+  import github from '../assets/github.svg';
+  import linkedin from '../assets/linkedin.svg';
+</script>
+
 <div class="mt-5 flex">
-    <a class="mx-2" href="https://t.me/moepoi" target="_blank">
-        <i class="ri-telegram-fill text-white text-2xl"></i>
-    </a>
-    <a class="mx-2" href="https://moe.onl/@moepoi" target="_blank">
-        <i class="ri-mastodon-fill text-white text-2xl"></i>
-    </a>
-    <a class="mx-2" href="https://github.com/moepoi" target="_blank">
-        <i class="ri-github-fill text-white text-2xl"></i>
-    </a>
-    <a class="mx-2" href="https://linkedin.com/in/moepoi" target="_blank">
-        <i class="ri-linkedin-box-fill text-white text-2xl"></i>
-    </a>
+  <a class="mx-2" href="https://t.me/moepoi" target="_blank">
+      <img class="w-8 h-8 text-white" src={telegram} alt="Telegram">
+  </a>
+  <a class="mx-2" href="https://moe.onl/@moepoi" target="_blank">
+      <img class="w-8 h-8 text-white" src={mastodon} alt="Mastodon">
+  </a>
+  <a class="mx-2" href="https://github.com/moepoi" target="_blank">
+      <img class="w-8 h-8 text-white" src={github} alt="GitHub">
+  </a>
+  <a class="mx-2" href="https://linkedin.com/in/moepoi" target="_blank">
+      <img class="w-8 h-8 text-white" src={linkedin} alt="LinkedIn">
+  </a>
 </div>
