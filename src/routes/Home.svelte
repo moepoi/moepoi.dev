b8a34992f94d574789585df93ec4120d4b65e8ca
Moe Poi ~ <moe@poi.lol>
2024-03-27 01:26:26 +0700

    Update Home.svelte styling

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -6,8 +6,8 @@
 
 <NavBar isFixed={true} />
 
-<div class="flex flex-col items-center justify-center min-h-screen text-white">
+<div class="flex flex-col items-center justify-center h-screen flex-wrap">
   <img class="w-40 h-40 rounded-full" src={profile} alt="" />
-  <h1 class="mt-5 text-4xl font-bold">Moepoi</h1>
+  <h1 class="mt-5 text-4xl text-white font-bold">Moepoi</h1>
   <SocialMedia />
 </div>
