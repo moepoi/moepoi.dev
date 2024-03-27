c1c92cb1e517c88e9afa0be19af9f6fc36515885
Moe Poi ~ <moe@poi.lol>
2024-03-27 15:00:20 +0700

    Update project

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -9,39 +9,36 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Nepgear Bot',
-    image: 'assets/images/projects/nepgear.webp',
-    date: '23 November 2018',
-    categories: 'Bot',
-    description:
-        'Telegram bot that allows you to search for your favorite Anime, Manga, Character with other useful features !!!',
-    url: 'https://t.me/NepgearBot'
-  },
-  {
-    title: 'Whois Bot',
-    image: 'assets/images/projects/whois.webp',
-    date: ' 3 September 2019',
-    categories: 'Bot',
-    description:
-        'Telegram bot that provide Information about domain, ip, and web hosting.',
-    url: 'https://t.me/WhoisBot'
-  },
-  {
-    title: 'Moe API',
-    image: 'assets/images/projects/moe-api.webp',
-    date: '8 November 2019',
-    categories: 'Web',
-    description: 'Powerful API with modern features !!!',
-    url: 'https://moe.team/'
-  },
-  {
-    title: 'Toaru Stickers',
-    image: 'assets/images/projects/toaru-stickers.webp',
-    date: '13 May 2020',
+    title: 'Kitchen Counts',
+    image: 'assets/images/projects/kitchen-counts.webp',
+    date: '6 October 2022',
     categories: 'App',
-    description:
-        'Toaru Stickers Application for Whatsapp. Build with Flutter :)',
-    url: 'https://github.com/moepoi/Toaru-Stickers'
+    description: 'Stock management app for kitchen.',
+    url: 'https://github.com/moepoi/Kitchen_Counts'
+  },
+  {
+    title: 'Nekoya App',
+    image: 'assets/images/projects/nekoya-app.webp',
+    date: '30 May 2022',
+    categories: 'App',
+    description: 'Native application for Nekoya ~',
+    url: 'https://github.com/Nekoya-Site/App'
+  },
+  {
+    title: 'Nekoya Web',
+    image: 'assets/images/projects/nekoya-web.webp',
+    date: '22 November 2021',
+    categories: 'Web',
+    description: 'Web application for Nekoya ~',
+    url: 'https://github.com/Nekoya-Site/Web'
+  },
+  {
+    title: 'Easy Learn',
+    image: 'assets/images/projects/easy-learn.webp',
+    date: '15 August 2021',
+    categories: 'App',
+    description: 'Learning app for kids.',
+    url: 'https://github.com/Easy-Learn/App'
   },
   {
     title: 'Neonime App',
@@ -53,35 +50,38 @@ export const projects: Project[] = [
     url: 'https://github.com/moepoi/Neonime-App'
   },
   {
-    title: 'Easy Learn',
-    image: 'assets/images/projects/easy-learn.webp',
-    date: '15 August 2021',
+    title: 'Toaru Stickers',
+    image: 'assets/images/projects/toaru-stickers.webp',
+    date: '13 May 2020',
     categories: 'App',
-    description: 'Learning app for kids.',
-    url: 'https://github.com/Easy-Learn/App'
+    description:
+        'Toaru Stickers Application for Whatsapp. Build with Flutter :)',
+    url: 'https://github.com/moepoi/Toaru-Stickers'
   },
   {
-    title: 'Nekoya Web',
-    image: 'assets/images/projects/nekoya-web.webp',
-    date: '22 November 2021',
+    title: 'Moe API',
+    image: 'assets/images/projects/moe-api.webp',
+    date: '8 November 2019',
     categories: 'Web',
-    description: 'Web application for Nekoya ~',
-    url: 'https://github.com/Nekoya-Site/Web'
+    description: 'Powerful API with modern features !!!',
+    url: 'https://moe.team/'
   },
   {
-    title: 'Nekoya App',
-    image: 'assets/images/projects/nekoya-app.webp',
-    date: '30 May 2022',
-    categories: 'App',
-    description: 'Native application for Nekoya ~',
-    url: 'https://github.com/Nekoya-Site/App'
+    title: 'Whois Bot',
+    image: 'assets/images/projects/whois.webp',
+    date: ' 3 September 2019',
+    categories: 'Bot',
+    description:
+        'Telegram bot that provide Information about domain, ip, and web hosting.',
+    url: 'https://t.me/WhoisBot'
   },
   {
-    title: 'Kitchen Counts',
-    image: 'assets/images/projects/kitchen-counts.webp',
-    date: '6 October 2022',
-    categories: 'App',
-    description: 'Stock management app for kitchen.',
-    url: 'https://github.com/moepoi/Kitchen_Counts'
-  },
+    title: 'Nepgear Bot',
+    image: 'assets/images/projects/nepgear.webp',
+    date: '23 November 2018',
+    categories: 'Bot',
+    description:
+        'Telegram bot that allows you to search for your favorite Anime, Manga, Character with other useful features !!!',
+    url: 'https://t.me/NepgearBot'
+  }
 ];
